b87d5e3a6be83f101ac27ca67235d88050bbc32c
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-02-06 00:30:44 +0900

    html_escape content of JSON

--- lib/Mgrgw/View/JSON.pm
+++ lib/Mgrgw/View/JSON.pm
@@ -1,8 +1,21 @@
 package Mgrgw::View::JSON;
 use Ark 'View::JSON';
+use Data::Rmap;
+use Text::MicroTemplate;
 
 has '+expose_stash' => (
     default => 'json',
+);
+
+has '+json_dumper' => (
+    default => sub {
+        my $self = shift;
+        sub {
+            my (@args) = @_;
+            rmap { $_ = Text::MicroTemplate::escape_html($_) } @args;
+            $self->json_driver->encode(@args);
+        };
+    }
 );
 
 1;
