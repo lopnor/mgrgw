6611b93a6d2d5bb1ed6e497728adb2000d1524a9
Nobuo Danjou <nobuo.danjou@gmail.com>
2010-08-22 18:00:13 +0900

    make current_appearance inline

--- lib/Mgrgw/Schema/ResultSet/Appearance.pm
+++ lib/Mgrgw/Schema/ResultSet/Appearance.pm
@@ -8,9 +8,21 @@ use DateTime;
 sub record {
     my ($self, $user, $address) = @_;
 
+    my $time = DateTime->now->add(minutes => -10);
+
     my $txn_guard = $self->result_source->schema->txn_scope_guard;
 
-    my $found = $self->current_appearance($user, $address);
+    my $found = $self->search(
+        {
+            user_id => $user->id,
+            address => $address,
+            updated_at => { '>=' => $time->strftime("%F %T") },
+        },
+        {
+            order_by => {-desc => 'updated_at'}
+        }
+    )->first;
+
     if ($found) {
         $found->update({updated_at => DateTime->now});
     } else {
@@ -23,20 +35,6 @@ sub record {
     }
     $txn_guard->commit;
     $found;
-}
-
-sub current_appearance {
-    my ($self, $user, $address) = @_;
-    $self->search(
-        {
-            user_id => $user->id,
-            address => $address,
-            updated_at => { '>=' => DateTime->now->add(minutes => -10)->strftime("%F %T") },
-        },
-        {
-            order_by => {-desc => 'updated_at'}
-        }
-    )->first;
 }
 
 1;
